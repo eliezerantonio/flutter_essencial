755fbc27f977ec2c2bdea5a5fee2441c95275d21
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-04-12 03:31:46 +0100

    scaffold

--- basico_e_widgets/lib/main.dart
+++ basico_e_widgets/lib/main.dart
@@ -1,19 +1,11 @@
+import 'package:basico_e_widgets/home_Screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Container(
-        color: Colors.white,
-      ),
-    );
-  }
+      home: HomeScreen(),
+    ),
+  );
 }
