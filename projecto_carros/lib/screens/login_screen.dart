2eba19dba929200808a06fc7237fcb8a09fb29ca
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-04-21 21:17:56 +0100

    TextEditingController

--- projecto_carros/lib/screens/login_screen.dart
+++ projecto_carros/lib/screens/login_screen.dart
@@ -3,6 +3,9 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
+  final _controllerLogin = TextEditingController();
+  final _controllerSenha = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,31 +25,35 @@ class LoginScreen extends StatelessWidget {
   ListView _formularioLogin() {
     return ListView(
       children: [
-        _text("Login", "Digite o Login"),
+        _text("Login", "Digite o Login", _controllerLogin),
         SizedBox(
           height: 20,
         ),
-        _text("Senha", "Digite a senha", obscure: true),
+        _text("Senha", "Digite a senha", _controllerSenha, obscure: true),
         SizedBox(
           height: 20,
         ),
-        _button("Login"),
+        _button("Login", () => _onClikLogin()),
       ],
     );
   }
 
-  Container _button(String text) {
+  Container _button(String text, Function onPressed) {
     return Container(
       height: 46,
       child: RaisedButton(
-        onPressed: () {},
+        color: Colors.blue,
+        textColor: Colors.white,
+        onPressed: onPressed,
         child: Text(text),
       ),
     );
   }
 
-  Widget _text(String label, String hint, {bool obscure = false}) {
+  Widget _text(String label, String hint, TextEditingController controller,
+      {bool obscure = false}) {
     return TextFormField(
+      controller: controller,
       obscureText: obscure,
       style: TextStyle(fontSize: 20, color: Colors.blue),
       decoration: InputDecoration(
@@ -56,5 +63,12 @@ class LoginScreen extends StatelessWidget {
             color: Colors.grey,
           )),
     );
+  }
+
+  _onClikLogin() {
+    String login = _controllerLogin.text;
+    String senha = _controllerSenha.text;
+
+    print(login + " " + senha);
   }
 }
