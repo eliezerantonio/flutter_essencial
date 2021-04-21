c8c2251615fb98270d29d21472144cbee40a5d7a
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-04-21 20:45:25 +0100

    loginScreen

---
+++ projecto_carros/lib/screens/login_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carros"),
+        centerTitle: true,
+      ),
+      body: Container(),
+    );
+  }
+}
