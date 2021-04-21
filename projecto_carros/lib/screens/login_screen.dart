2336e8bd9f1c63760cc65a117d1b04b890793c27
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-04-21 20:55:25 +0100

    formulario login

--- projecto_carros/lib/screens/login_screen.dart
+++ projecto_carros/lib/screens/login_screen.dart
@@ -8,7 +8,43 @@ class LoginScreen extends StatelessWidget {
         title: Text("Carros"),
         centerTitle: true,
       ),
-      body: Container(),
+      body: _body(),
+    );
+  }
+
+  Container _body() => Container(
+        padding: EdgeInsets.all(16),
+        child: _formularioLogin(),
+      );
+
+  ListView _formularioLogin() {
+    return ListView(
+      children: [
+        TextFormField(
+          decoration: InputDecoration(
+            labelText: "Login",
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        TextFormField(
+          obscureText: true,
+          decoration: InputDecoration(
+            labelText: "Password",
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Container(
+          height: 46,
+          child: RaisedButton(
+            onPressed: () {},
+            child: Text("Login"),
+          ),
+        ),
+      ],
     );
   }
 }
