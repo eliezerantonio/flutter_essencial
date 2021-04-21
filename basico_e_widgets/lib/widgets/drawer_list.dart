dd9b7976e554da174eebe2040e21fbe0e9b908ab
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-04-21 20:05:59 +0100

    SafeArea

--- basico_e_widgets/lib/widgets/drawer_list.dart
+++ basico_e_widgets/lib/widgets/drawer_list.dart
@@ -5,43 +5,45 @@ class DrawerList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        children: [
-          UserAccountsDrawerHeader(
-            accountName: Text("Eliezer Antonio"),
-            accountEmail: Text("eliezer@gmail.com"),
-            currentAccountPicture: CircleAvatar(
-              backgroundImage: AssetImage("assets/images/dog1.png"),
+    return SafeArea(
+      child: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text("Eliezer Antonio"),
+              accountEmail: Text("eliezer@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage("assets/images/dog1.png"),
+              ),
             ),
-          ),
-          ListTile(
-            leading: Icon(Icons.star),
-            title: Text("Subtitulo"),
-            subtitle: Text("Mais informacoes"),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.help),
-            title: Text("Ajuda"),
-            subtitle: Text("Mais informacoes"),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.exit_to_app),
-            title: Text("Exit"),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-        ],
+            ListTile(
+              leading: Icon(Icons.star),
+              title: Text("Subtitulo"),
+              subtitle: Text("Mais informacoes"),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.help),
+              title: Text("Ajuda"),
+              subtitle: Text("Mais informacoes"),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text("Exit"),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
