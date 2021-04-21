2e945977657c9d792c4de20b87141f50627db521
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-04-21 20:02:37 +0100

    drawer

--- basico_e_widgets/lib/home_Screen.dart
+++ basico_e_widgets/lib/home_Screen.dart
@@ -7,13 +7,15 @@ import 'package:basico_e_widgets/widgets/blue_button.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
+import 'widgets/drawer_list.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: Drawer(),
+        drawer: DrawerList(),
         appBar: AppBar(
           title: Text("Hello flutter"),
         ),
---
+++ basico_e_widgets/lib/widgets/drawer_list.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class DrawerList extends StatelessWidget {
+  const DrawerList({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          ListTile(
+            leading: Icon(Icons.star),
+            title: Text("Subtitulo"),
+            subtitle: Text("Mais informacoes"),
+            trailing: Icon(Icons.arrow_forward),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.help),
+            title: Text("Ajuda"),
+            subtitle: Text("Mais informacoes"),
+            trailing: Icon(Icons.arrow_forward),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text("Exit"),
+            trailing: Icon(Icons.arrow_forward),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
