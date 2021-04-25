bb1ce93c592efb77cda958787212c89469694eb1
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-04-25 14:26:42 +0100

    ButtonTheme.bar

--- projecto_carros/lib/carro/home_screen.dart
+++ projecto_carros/lib/carro/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:projecto_carros/carro/carros_api.dart';
 import 'package:projecto_carros/widgets/drawer_list.dart';
+
+import 'carro.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key key}) : super(key: key);
@@ -16,6 +19,57 @@ class HomeScreen extends StatelessWidget {
   }
 
   _body() {
-    return Text("Eliezer Antonio");
+    List<Carro> carros = CarrosApi.getCarros();
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ListView.builder(
+        itemCount: carros.length,
+        itemBuilder: (context, index) {
+          Carro c = carros[index];
+
+          return Card(
+            color: Colors.grey[100],
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Image.network(
+                      c.urlFoto,
+                      width: 250,
+                    ),
+                  ),
+                  Text(
+                    c.nome,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  Text(
+                    "Descricao..",
+                    style: TextStyle(fontSize: 17),
+                  ),
+                  ButtonTheme.bar(
+                    child: ButtonBar(
+                      children: [
+                        FlatButton(
+                          onPressed: () {},
+                          child: Text("DETALHES"),
+                        ),
+                        FlatButton(
+                          onPressed: () {},
+                          child: Text("SHARE"),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
