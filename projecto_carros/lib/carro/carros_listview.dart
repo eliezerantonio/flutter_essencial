549a22c5d4434408b90d116990c4eda47ec3d43c
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-05-07 20:49:23 +0100

    refactor o carro

---
+++ projecto_carros/lib/carro/carros_listview.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+import 'carro.dart';
+import 'carros_api.dart';
+
+class CarrosListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _body(),
+    );
+  }
+
+  _body() {
+    Future<List<Carro>> carros = CarrosApi.getCarros();
+    return FutureBuilder(
+      future: carros,
+      builder: (context, snapshot) {
+        List<Carro> carros = snapshot.data;
+        if (snapshot.hasError) {
+          return Center(
+            child: Text(
+              "Impossivel buscar carros",
+              style: TextStyle(color: Colors.red, fontSize: 22),
+            ),
+          );
+        }
+
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return _listView(carros);
+      },
+    );
+  }
+
+  Padding _listView(List<Carro> carros) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ListView.builder(
+        itemCount: carros != null ? carros.length : 0,
+        itemBuilder: (context, index) {
+          Carro c = carros[index];
+
+          return Card(
+            color: Colors.grey[100],
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Image.network(
+                      c.urlFoto ??
+                          "https://s3-sa-east-1.amazonaws.com/videos.livetouchdev.com.br/classicos/Chevrolet_Impala_Coupe.png",
+                      width: 250,
+                    ),
+                  ),
+                  Text(
+                    c.nome,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  Text(
+                    "Descricao..",
+                    style: TextStyle(fontSize: 17),
+                  ),
+                  ButtonBarTheme(
+                    data: ButtonBarTheme.of(context),
+                    child: ButtonBar(
+                      children: <Widget>[
+                        FlatButton(
+                          child: const Text('DETALHES'),
+                          onPressed: () {
+                            /* ... */
+                          },
+                        ),
+                        FlatButton(
+                          child: const Text('SHARE'),
+                          onPressed: () {
+                            /* ... */
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- projecto_carros/lib/carro/home_screen.dart
+++ projecto_carros/lib/carro/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:projecto_carros/carro/carros_api.dart';
+import 'package:projecto_carros/carro/carros_listview.dart';
 import 'package:projecto_carros/widgets/drawer_list.dart';
 
 import 'carro.dart';
@@ -14,92 +15,7 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Carros"),
       ),
-      body: _body(),
-    );
-  }
-
-  _body() {
-    Future<List<Carro>> carros = CarrosApi.getCarros();
-    return FutureBuilder(
-      future: carros,
-      builder: (context, snapshot) {
-        List<Carro> carros = snapshot.data;
-        if (snapshot.hasError) {
-          return Center(
-            child: Text(
-              "Impossivel buscar carros",
-              style: TextStyle(color: Colors.red, fontSize: 22),
-            ),
-          );
-        }
-
-        if (!snapshot.hasData) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        return _listView(carros);
-      },
-    );
-  }
-
-  Padding _listView(List<Carro> carros) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: ListView.builder(
-        itemCount: carros != null ? carros.length : 0,
-        itemBuilder: (context, index) {
-          Carro c = carros[index];
-
-          return Card(
-            color: Colors.grey[100],
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Center(
-                    child: Image.network(
-                      c.urlFoto ??
-                          "https://s3-sa-east-1.amazonaws.com/videos.livetouchdev.com.br/classicos/Chevrolet_Impala_Coupe.png",
-                      width: 250,
-                    ),
-                  ),
-                  Text(
-                    c.nome,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(fontSize: 25),
-                  ),
-                  Text(
-                    "Descricao..",
-                    style: TextStyle(fontSize: 17),
-                  ),
-                  ButtonBarTheme(
-                    data: ButtonBarTheme.of(context),
-                    child: ButtonBar(
-                      children: <Widget>[
-                        FlatButton(
-                          child: const Text('DETALHES'),
-                          onPressed: () {
-                            /* ... */
-                          },
-                        ),
-                        FlatButton(
-                          child: const Text('SHARE'),
-                          onPressed: () {
-                            /* ... */
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
+      body: CarrosListView(),
     );
   }
 }
