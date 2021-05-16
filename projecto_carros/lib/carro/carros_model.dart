3604c5cc67abdc5dc631f3065aa4b59e275aa0ac
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-05-15 21:30:48 -0400

    mobx

---
+++ projecto_carros/lib/carro/carros_model.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+
+import 'package:mobx/mobx.dart';
+import 'package:projecto_carros/carro/carros_api.dart';
+import 'package:projecto_carros/carro/simple_bloc.dart';
+
+import 'carro.dart';
+
+part 'carros_model.g.dart';
+
+class CarrosModel = CarrosModelBase with _$CarrosModel;
+
+abstract class CarrosModelBase with Store {
+  @observable
+  List<Carro> carros;
+
+  @observable
+  Exception error;
+
+  @action //nos metodos que vao alterar a interface
+  fetch(TipoCarro tipoCarro) async {
+    try {
+      this.carros = await CarrosApi.getCarros(tipoCarro);
+    } catch (e) {
+      error = e;
+    }
+  }
+}
