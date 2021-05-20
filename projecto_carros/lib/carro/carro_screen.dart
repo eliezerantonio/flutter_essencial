98794b1a270fdd784ee0ddbef9fbad8c42b4656c
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-05-20 22:25:35 +0100

    PopupMenuButton

--- projecto_carros/lib/carro/carro_screen.dart
+++ projecto_carros/lib/carro/carro_screen.dart
@@ -9,6 +9,35 @@ class CarroScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(carro.nome),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.place),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.videocam),
+            onPressed: () {},
+          ),
+          PopupMenuButton<String>(
+            itemBuilder: (BuildContext context) {
+              return [
+                PopupMenuItem(
+                  value: "Editar",
+                  child: Text("Editar"),
+                ),
+                PopupMenuItem(
+                  value: "Deletar",
+                  child: Text("Deletar"),
+                ),
+                PopupMenuItem(
+                  value: "Share",
+                  child: Text("Share"),
+                ),
+              ];
+            },
+            onSelected: _onClickPopupMenu,
+          ),
+        ],
       ),
       body: _body(),
     );
@@ -19,5 +48,19 @@ class CarroScreen extends StatelessWidget {
       padding: EdgeInsets.all(16),
       child: Image.network(carro.urlFoto),
     );
+  }
+
+  _onClickPopupMenu(String value) {
+    switch (value) {
+      case "Editar":
+        print("editar");
+        break;
+      case "Deletar":
+        print("Deletar");
+        break;
+      case "Share":
+        print("Share");
+        break;
+    }
   }
 }
