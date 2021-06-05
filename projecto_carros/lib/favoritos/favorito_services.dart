ce613dc445946e9dfba43eb9321828464c3ed581
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-06-05 08:54:42 +0100

    removendo favoritos

--- projecto_carros/lib/favoritos/favorito_services.dart
+++ projecto_carros/lib/favoritos/favorito_services.dart
@@ -3,9 +3,17 @@ import 'package:projecto_carros/favoritos/favorito.dart';
 import 'package:projecto_carros/favoritos/favorito_dao.dart';
 
 class FavoritoService {
-  static favoritar(Carro c) {
+  static favoritar(Carro c) async {
     Favorito f = Favorito.fromCarro(c);
 
     final dao = FavoritoDAO();
+
+    final exists = await dao.exists(c.id);
+
+    if (exists) {
+      dao.delete(c.id);
+    } else {
+      dao.save(f);
+    }
   }
 }
