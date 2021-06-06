1236a714e1bc6d95a3a6c401a48caca4906b1005
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-06-06 11:54:36 +0100

    camera

---
+++ hello_camera/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:hello_camera/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
