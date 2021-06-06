cd38395fafc990b292edbbd0467f719f226012a0
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-06-06 10:26:28 +0100

    http helper

--- projecto_carros/lib/carro/carros_api.dart
+++ projecto_carros/lib/carro/carros_api.dart
@@ -1,9 +1,8 @@
 import 'package:projecto_carros/carro/carro.dart';
-import 'package:http/http.dart' as http;
 import 'package:projecto_carros/helpers/api_response.dart';
-import 'package:projecto_carros/helpers/sql/base_dao.dart';
 import 'package:projecto_carros/carro/carro_dao.dart';
 import 'dart:convert' as convert;
+import 'package:projecto_carros/helpers/http_helper.dart' as http;
 
 import 'package:projecto_carros/usuario/usuario.dart';
 
@@ -11,18 +10,14 @@ enum TipoCarro { classicos, esportivos, luxo }
 
 class CarrosApi {
   static Future<List<Carro>> getCarros(TipoCarro tipoCarro) async {
-    Usuario user = await Usuario.get();
-    Map<String, String> headers = {
-      "Content-type": "application/json",
-      "Authorization": "Bearer ${user.token}",
-    };
+   ;
 
     String tipo = tipoCarro.toString().replaceAll("TipoCarro.", "");
     var url =
         'https://carros-springboot.herokuapp.com/api/v2/carros/tipo/$tipo';
 
     print("GET $url");
-    var response = await http.get(url, headers: headers);
+    var response = await http.get(url, );
     String json = response.body;
 
     List list = convert.json.decode(json);
@@ -34,13 +29,7 @@ class CarrosApi {
   }
 
   static Future<ApiResponse<bool>> save(Carro c) async {
-    Usuario user = await Usuario.get();
-
-    print(" token ${user.token}");
-    Map<String, String> headers = {
-      "Content-type": "application/json",
-      "Authorization": "Bearer ${user.token}",
-    };
+  
 
     var url = 'https://carros-springboot.herokuapp.com/api/v2/carros';
     if (c.id != null) {
@@ -52,8 +41,8 @@ class CarrosApi {
     String json = c.toJsonC();
 
     var response = await (c.id == null
-        ? http.post(url, body: json, headers: headers)
-        : http.put(url, body: json, headers: headers));
+        ? http.post(url, body: json, )
+        : http.put(url, body: json, ));
 
     print("reponse status: ${response.statusCode} ");
     print("response body: ${response.body}");
@@ -75,31 +64,20 @@ class CarrosApi {
   }
 
   static Future<ApiResponse<bool>> delete(Carro c) async {
-    Usuario user = await Usuario.get();
-
-    print(" token ${user.token}");
-    Map<String, String> headers = {
-      "Content-type": "application/json",
-      "Authorization": "Bearer ${user.token}",
-    };
-
+   
     var url = 'https://carros-springboot.herokuapp.com/api/v2/carros/${c.id}';
 
     print("POST > $url");
 
-   
-    var response = await
-         http.delete(url,  headers: headers);
+    var response = await http.delete(url,);
 
-    print("reponse status: ${response.statusCode} ");
+    print("reponse status: ${response.statusCode}");
     print("response body: ${response.body}");
 
     if (response.statusCode == 201 || response.statusCode == 200) {
-  
-
       return ApiResponse.ok(true);
     }
-  
+
     return ApiResponse.error("Nao foi possivel apagar o carro");
   }
 }
